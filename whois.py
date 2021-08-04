73d1bb8d1a602b483925f124d1cd7949efc68ed9
Burkhard84 <88413591+Burkhard84@users.noreply.github.com>
2021-08-04 00:30:59 -0300

    Update whois.py

--- whois.py
+++ whois.py
@@ -34,9 +34,10 @@ print(r"""**********************************************************************
                      e regras para o registro de um domínio. 
                   
     IP ou bloco CIDR: digite um número IP (200.200.200.200) ou um bloco CIDR (200.200/16).
-                                                                                                    
-                             exemplo de uso python3 whois.py 
-                                                                                                    """)
+                                                                               
+                              abrir o programa : python3 whois.py                                                  
+                             exemplo de uso: Digite Domínio ou IP: exemplo.com                              
+                             exemplo de uso: Digite Domínio ou IP: 127.0.0.1                        """)
 print("\n*********************************************************************************************")
 
 import socket
